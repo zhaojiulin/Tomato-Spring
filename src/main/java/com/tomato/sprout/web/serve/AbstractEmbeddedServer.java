19406d919b3f653e68b65b02afdf5c79c31fa567
bunana <jiulinz@163.com>
2025-10-22 17:27:41 +0800

    描述：扩展内嵌服务器 时间：2025.10.22

---
+++ src/main/java/com/tomato/sprout/web/serve/AbstractEmbeddedServer.java
@@ -0,0 +1,10 @@
+package com.tomato.sprout.web.serve;
+
+/**
+ * @author zhaojiulin
+ * @version 1.0
+ * @description: 服务器抽象类
+ * @date 2025/10/22 17:26
+ */
+public abstract class AbstractEmbeddedServer {
+}
---
+++ src/main/java/com/tomato/sprout/web/serve/EmbeddedServerFactory.java
@@ -0,0 +1,10 @@
+package com.tomato.sprout.web.serve;
+
+/**
+ * @author zhaojiulin
+ * @version 1.0
+ * @description: 内嵌服务器工厂类
+ * @date 2025/10/22 17:24
+ */
+public class EmbeddedServerFactory {
+}
